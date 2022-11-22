33362aa70ff1bf082af00c0ac9ce6f9981fdc5e7
satyank <satyank.nadimpalli@outlook.com>
2022-11-21 19:35:59 -0600

    Updated Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { CustomerLoginComponent } from './customer-login/customer-login.componen
 import { CustomerDashboardComponent } from './customer-dashboard/customer-dashboard.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
+
 
 @NgModule({
   declarations: [
